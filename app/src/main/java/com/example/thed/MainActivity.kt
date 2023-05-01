13fdab7915bb57794e27615ce0abf1e27987e505
chill.shi <chill.shi@pointroboticsinc.com>
2023-05-01 10:21:23 +0800

    randomly generate dice

--- app/src/main/java/com/example/thed/MainActivity.kt
+++ app/src/main/java/com/example/thed/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.thed
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.thed.databinding.ActivityMainBinding
+import kotlinx.coroutines.yield
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var mainBinding: ActivityMainBinding
@@ -15,7 +17,11 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         mainBinding.recyclerView.adapter = rollAdapter
         mainBinding.button.setOnClickListener {
-            rollAdapter.rollList.add(0, listOf(1, 2, 3, 4, 5))
+            rollAdapter.rollList.add(0, sequence {
+                for (i in 0 until diceNum){
+                    yield(Random.Default.nextInt(1, 7))
+                }
+            }.toList())
             rollAdapter.notifyItemInserted(0)
             mainBinding.recyclerView.scrollToPosition(0)
         }
